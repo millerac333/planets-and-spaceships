dcd98415f5ba7bbd664f41f23227d5012755b288
Aaron Miller <millerac333@gmail.com>
2018-08-22 11:32:16 -0500

    added console.write line

--- Program.cs
+++ Program.cs
@@ -28,26 +28,13 @@ namespace lists
             planetList.Insert(2, "Earth");
             planetList.Insert(3, "Venus");
 
-            //
-            // Summary:
-            //     Inserts the elements of a collection into the System.Collections.Generic.List`1
-            //     at the specified index.
-            //
-            // Parameters:
-            //   index:
-            //     The zero-based index at which the new elements should be inserted.
-            //
-            //   collection:
-            //     The collection whose elements should be inserted into the System.Collections.Generic.List`1.
-            //     The collection itself cannot be null, but it can contain elements that are null,
-            //     if type T is a reference type.
-            //
-            // Exceptions:
-            //   T:System.ArgumentNullException:
-            //     collection is null.
-            //
-            //   T:System.ArgumentOutOfRangeException:
-            //     index is less than 0. -or- index is greater than System.Collections.Generic.List`1.Count.
+            foreach (string item in planetList)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
 
 
         }
